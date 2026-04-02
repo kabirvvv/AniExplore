417f21110eafa0e27019031a68e8f12efa7824d2
kabirvvv <kabirv338@gmail.com>
2026-04-02 13:41:22 +0530

    Update grok.js

--- api/grok.js
+++ api/grok.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
         Authorization: `Bearer ${GROK_API_KEY}`,
       },
       body: JSON.stringify({
-        model: "grok-2-latest",
+        model: "grok-2-1212",
         max_tokens: 1000,
         messages: [
           {
