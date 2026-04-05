9cccedf0a82163c1310c7c96418211c0c1c248ff
kabirvvv <kabirv338@gmail.com>
2026-04-05 18:41:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
+   
   server: {
     port: 5173,
     // Proxy /api calls to Vercel dev server locally
